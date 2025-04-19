c4c7bd36248c2c0f0bd69e378497c2da09395026
lihoubrak <blh.braklihou18@gmail.com>
2025-04-19 10:19:27 +0700

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Constants
-GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfCW_Qip3UPXGbnHbC7SzmaNkHz3BWNPeleCVm3-XDLrDxoLw/viewform"
-EXCEL_FILE = "test copy.xlsx"
+GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeqWvnn4KIru5BYd6aNVTCvaej6KvPWdbK0tN3piOgU8u8ftg/viewform"
+EXCEL_FILE = "test - Copy.xlsx"
 CHROMEDRIVER_PATH = r"D:\trc_auto\chromedriver.exe"
 SIMILARITY_THRESHOLD = 80
 USER_DATA_DIR = r"C:\Users\user\AppData\Local\Google\Chrome\User Data"
